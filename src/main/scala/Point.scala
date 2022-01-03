9f3f8310ae41ee1a97d332cf126451194a7d1559
jean-94 <jean.ly@efrei.net>
2022-01-03 15:20:28 +0100

    "Test on deserialisation Point"

--- src/main/scala/Point.scala
+++ src/main/scala/Point.scala
@@ -4,8 +4,8 @@ case class Point2D private(x: Float, y: Float) extends Point{}
 case class Point3D private(x: Float, y: Float, z: Float) extends Point{}
 
 object Point {
-  def newPoint2D(x: Float, y: Float): Point = Point2D(x,y)
-  def newPoint3D(x: Float, y: Float, z: Float): Point = Point3D(x,y,z)
+  def newPoint2D(x: Float, y: Float): Point2D = Point2D(x,y)
+  def newPoint3D(x: Float, y: Float, z: Float): Point3D = Point3D(x,y,z)
 
   def deserialization(line: String,sep: String = ","): Either[String,Point] = {
     val pattern2 = ("""^[+-]?[0-9]+\.?[0-9]*\""" +
--- src/test/scala/Test_Point.scala
+++ src/test/scala/Test_Point.scala
@@ -3,7 +3,7 @@ import Point.newPoint2D
 class Test_Point extends munit.FunSuite{
   test("Testing 2D point"){
     val p = newPoint2D(1,2)
-    assertEquals(p.getX,1)
-    assertEquals(p.getY,2)
+    assertEquals(p.x,1.toFloat)
+    assertEquals(p.y,2.toFloat)
   }
 }
