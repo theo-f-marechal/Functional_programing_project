7e08060813a51fba13282c9015244b18cae10e78
theo <theo.francois.marechal@gmail.com>
2022-01-17 19:57:09 +0100

    runways_csv structure now mostly working

--- src/main/scala/Structure_CSV/runways_csv.scala
+++ src/main/scala/Structure_CSV/runways_csv.scala
@@ -1,186 +1,119 @@
 package Structure_CSV
 
-case class runways_csv(id : Option[id],
-                        airport_ref : Option[airport_ref],
-                        airport_ident : Option[airport_ident],
-                        length_ft : Option[length_ft],
-                        width_ft : Option[width_ft],
-                        surface : Option[surface],
-                        lighted : Option[lighted],
-                        closed : Option[closed],
-                        le_ident : Option[le_ident],
-                        le_latitude_deg : Option[le_latitude_deg],
-                        le_longitude_deg : Option[le_longitude_deg],
-                        le_elevation_ft : Option[le_elevation_ft],
-                        le_heading_degT : Option[le_heading_degT],
-                        le_displaced_threshold_ft : Option[le_displaced_threshold_ft],
-                        he_ident : Option[he_ident],
-                        he_latitude_deg : Option[he_latitude_deg],
-                        he_longitude_deg : Option[he_longitude_deg],
-                        he_elevation_ft : Option[he_elevation_ft],
-                        he_heading_degT : Option[he_heading_degT],
-                        he_displaced_threshold_ft : Option[he_displaced_threshold_ft])
+import scala.annotation.tailrec
+
+sealed trait Runway_argument
+
+case class Runways_csv(id : Runaway_id,
+                        airport_ref : Airport_ref,
+                        airport_ident : Airport_ident,
+                        length_ft : Option[Length_ft],
+                        width_ft : Option[Width_ft],
+                        surface : Option[Surface],
+                        lighted : Option[Lighted],
+                        closed : Option[Closed],
+                        le_ident : Option[Le_ident])
 
 
-case class id private(id: Long) extends AnyVal
-object id {
-  def new_id(x: Option[Long]): Option[id] = x match {
-    case None => None
-    case _ if x.get < 0 => None
-    case _ => Some(id(x.get))
+object Runways_csv{
+  def deserialization(args: List[String]): Either[List[String], Runways_csv] = {
+    @tailrec
+    def transformArgs[Runway_argument](args: List[String],
+                                       listF: List[String => Option[Runway_argument]],
+                                       listArgT: List[Option[Runway_argument]] = Nil):
+    List[Option[Runway_argument]] = {
+      (args,listF,listArgT) match {
+        case (Nil,_,Nil) => List()
+        case (Nil,_,_) => listArgT.reverse
+        case (arg::tArg,function::tF,_) => transformArgs(tArg,tF,function(arg)::listArgT)
+      }
+    }
+
+    @tailrec
+    def checkCondition(argT : List[Option[Runway_argument]],
+                       possibleErrors: List[String],
+                       errorsFound : List[String] = Nil): List[String] =
+      (argT, possibleErrors) match {
+        case (Nil,_) => errorsFound.reverse
+        case (None::t1, h::t2) => checkCondition(t1,t2,h::errorsFound)
+        case (_::t1, _::t2) => checkCondition(t1,t2,errorsFound)
+    }
+
+    def makeOptionalArgument[A,B](fun : A => B, x : Option[A]) : Option[B] = x match{
+      case None => None
+      case Some(v) => Some(fun(v))
+    }
+
+    val listF = List(
+      {x : String => Runaway_id.new_id(x.toLongOption)},
+      {x : String => Airport_ref.new_airport_ref(x.toLongOption)},
+      {x : String => Airport_ident.new_airport_ident(Option(x))},
+      {x : String => makeOptionalArgument(Length_ft,x.toIntOption)},
+      {x : String => makeOptionalArgument(Width_ft,x.toIntOption)},
+      {x : String => makeOptionalArgument(Surface,Option(x))},
+      {x : String => makeOptionalArgument(Lighted,x.toIntOption)},
+      {x : String => makeOptionalArgument(Closed,x.toIntOption)},
+      {x : String => makeOptionalArgument(Le_ident,Option(x))})
+    val argsT = transformArgs(args,listF)
+
+    val possibleErrors = List(
+      "Missing id", "Missing ref", "Missing ident",
+      "Missing length_ft", "Missing width_ft",
+      "Missing surface", "Missing lighted",
+      "Missing closed", "Missing le_ident")
+
+    val errors = checkCondition(argsT, possibleErrors)
+
+    def liftOptionalArg(t : List[Option[Runway_argument]], i : Int): Option[Runway_argument] = {
+      val re = t.lift(i)
+      re match {
+        case None => None
+        case Some(x) => x
+      }
+    }
+
+    argsT match {
+      case Nil => Left("No argument given" :: errors)
+      case l if l.length < 3 => Left(errors)
+      case a::b::c::_ if a.isEmpty | b.isEmpty | c.isEmpty => Left(errors)
+      case Some(id : Runaway_id)::Some(ref : Airport_ref)::Some(ident : Airport_ident) ::
+        Some(lenght : Length_ft) :: Some(width : Width_ft) :: Some(surface : Surface) ::
+        Some(light : Lighted) :: Some(closed : Closed) :: Some(le_ident : Le_ident) :: _
+      => Right(Runways_csv(id,ref,ident,Some(lenght),Some(width),Some(surface),Some(light),Some(closed),Some(le_ident)))
+    }
   }
 }
 
-case class airport_ref private(ref: Long) extends AnyVal
-object airport_ref {
-  def new_airport_ref(x: Option[Long]): Option[airport_ref] = x match {
+
+
+case class Runaway_id private(id: Long) extends AnyVal with Runway_argument
+object Runaway_id {
+  def new_id(id: Option[Long]): Option[Runaway_id] = id match {
     case None => None
-    case _ => Some(airport_ref(x.get))
+    case Some(x) if x < 0 => None
+    case Some(x) => Some(Runaway_id(x))
   }
 }
 
-case class airport_ident private(ident: String) extends AnyVal
-object airport_ident {
-  def new_airport_ident(x: Option[String]): Option[airport_ident] = x match {
+case class Airport_ref private(ref: Long) extends AnyVal with Runway_argument
+object Airport_ref {
+  def new_airport_ref(ref: Option[Long]): Option[Airport_ref] = ref match {
     case None => None
-    case _ => Some(airport_ident(x.get))
+    case Some(x) => Some(Airport_ref(x))
   }
 }
 
-case class length_ft private(length: Float) extends AnyVal
-object length_ft {
-  def new_length_ft(x: Option[Float]): Option[length_ft] = x match {
+case class Airport_ident private(ident: String) extends AnyVal with Runway_argument
+object Airport_ident {
+  def new_airport_ident(ident: Option[String]): Option[Airport_ident] = ident match {
     case None => None
-    case _ if x.get < 0 => None
-    case _ => Some(length_ft(x.get))
+    case Some(x) => Some(Airport_ident(x))
   }
 }
 
-case class width_ft private(width: Float) extends AnyVal
-object width_ft {
-  def new_width_ft(x: Option[Float]): Option[width_ft] = x match {
-    case None => None
-    case _ if x.get < 0 => None
-    case _ => Some(width_ft(x.get))
-  }
-}
-
-case class surface private(typeSurface: String) extends AnyVal
-object surface {
-  def new_surface(x: Option[String]): Option[surface] = x match {
-    case None => None
-    case _ => Some(surface(x.get))
-  }
-}
-
-case class lighted private(light: Int) extends AnyVal
-object lighted {
-  def new_lighted(x: Option[String]): Option[airport_ident] = x match {
-    case None => None
-    case _ => Some(airport_ident(x.get))
-  }
-}
-
-case class closed private(close: Int) extends AnyVal
-object closed {
-  def new_closed(x: Option[Int]): Option[closed] = x match {
-    case None => None
-    case _ => Some(closed(x.get))
-  }
-}
-
-case class le_ident private(indent: Float) extends AnyVal
-object le_ident {
-  def new_le_ident(x: Option[Float]): Option[le_ident] = x match {
-    case None => None
-    case _ => Some(le_ident(x.get))
-  }
-}
-
-case class le_latitude_deg private(lat_d: Float) extends AnyVal
-object le_latitude_deg {
-  def new_le_latitude_deg(x: Option[Float]): Option[le_latitude_deg] = x match {
-    case None => None
-    case _ => Some(le_latitude_deg(x.get))
-  }
-}
-
-case class le_longitude_deg private(long_d: Float) extends AnyVal
-object le_longitude_deg {
-  def new_le_longitude_deg(x: Option[Float]): Option[le_longitude_deg] = x match {
-    case None => None
-    case _ => Some(le_longitude_deg(x.get))
-  }
-}
-
-case class le_elevation_ft private(elevation: Float) extends AnyVal
-object le_elevation_ft {
-  def new_le_elevation_ft(x: Option[Float]): Option[le_elevation_ft] = x match {
-    case None => None
-    case _ => Some(le_elevation_ft(x.get))
-  }
-}
-
-case class le_heading_degT private(heading: Float) extends AnyVal
-object le_heading_degT {
-  def new_le_heading_degT(x: Option[Float]): Option[le_heading_degT] = x match {
-    case None => None
-    case _ => Some(le_heading_degT(x.get))
-  }
-}
-
-case class le_displaced_threshold_ft private(displaced_thr: Float) extends AnyVal
-object le_displaced_threshold_ft {
-  def new_le_displaced_threshold_ft(x: Option[Float]): Option[le_displaced_threshold_ft] = x match {
-    case None => None
-    case _ => Some(le_displaced_threshold_ft(x.get))
-  }
-}
-
-case class he_ident private(indent: Float) extends AnyVal
-object he_ident {
-  def new_he_ident(x: Option[Float]): Option[he_ident] = x match {
-    case None => None
-    case _ => Some(he_ident(x.get))
-  }
-}
-
-case class he_latitude_deg private(lat_d: Float) extends AnyVal
-object he_latitude_deg {
-  def new_he_latitude_deg(x: Option[Float]): Option[he_latitude_deg] = x match {
-    case None => None
-    case _ => Some(he_latitude_deg(x.get))
-  }
-}
-
-case class he_longitude_deg private(lon_d: Float) extends AnyVal
-object he_longitude_deg {
-  def new_he_longitude_deg(x: Option[Float]): Option[he_longitude_deg] = x match {
-    case None => None
-    case _ => Some(he_longitude_deg(x.get))
-  }
-}
-
-case class he_elevation_ft private(elevation: Float) extends AnyVal
-object he_elevation_ft {
-  def new_he_elevation_ft(x: Option[Float]): Option[he_elevation_ft] = x match {
-    case None => None
-    case _ => Some(he_elevation_ft(x.get))
-  }
-}
-
-case class he_heading_degT private(heading: Float) extends AnyVal
-object he_heading_degT {
-  def new_he_heading_degT(x: Option[Float]): Option[he_heading_degT] = x match {
-    case None => None
-    case _ => Some(he_heading_degT(x.get))
-  }
-}
-
-case class he_displaced_threshold_ft private(displaced_thr: Float) extends AnyVal
-object he_displaced_threshold_ft {
-  def new_he_displaced_threshold_ft(x: Option[Float]): Option[he_displaced_threshold_ft] = x match {
-    case None => None
-    case _ => Some(he_displaced_threshold_ft(x.get))
-  }
-}
+case class Length_ft(len : Int) extends AnyVal with Runway_argument
+case class Width_ft(len : Int) extends AnyVal with Runway_argument
+case class Surface(len : String) extends AnyVal with Runway_argument
+case class Lighted(len : Int) extends AnyVal with Runway_argument
+case class Closed(len : Int) extends AnyVal with Runway_argument
+case class Le_ident(len : String) extends AnyVal with Runway_argument
