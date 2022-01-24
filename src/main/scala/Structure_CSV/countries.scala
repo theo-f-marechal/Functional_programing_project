4c2f1d0e84265c6387f4173f25488f39ebcebbf0
jean-94 <jean.ly@efrei.net>
2022-01-24 14:33:27 +0100

    "Update Countries class, variables, Object to camel case"

--- src/main/scala/Structure_CSV/countries.scala
+++ src/main/scala/Structure_CSV/countries.scala
@@ -1,23 +1,23 @@
 package Structure_CSV
 
-case class countries_csv(id : Country_id,
+case class countries_csv(id : CountryId,
                          code : Code,
                          name : String,
                          continent : Option[Continent],
-                         internet_link : Option[Internet_link],
+                         internetLink : Option[InternetLink],
                          keywords : Option[String])
 
-case class Country_id private(Id: Long) extends AnyVal
+case class CountryId private(id: Long) extends AnyVal
 object Id {
-  def new_id(x: Option[Long]): Option[Country_id] = x match {
+  def new_id(x: Option[Long]): Option[CountryId] = x match {
     case None => None
     case Some(x) if x < 0 => None
-    case Some(x) => Some(Country_id(x))
+    case Some(x) => Some(CountryId(x))
   }
 }
 
-case class Code private(code_name: String) extends AnyVal
-object code {
+case class Code private(codeName: String) extends AnyVal
+object Code {
   def new_code(x: Option[String]): Option[Code] = {
     val pattern = """^[A-Z]{2}$""".r
     x match {
@@ -30,8 +30,8 @@ object code {
   }
 }
 
-case class Continent private(continent_name: String) extends AnyVal
-object continent {
+case class Continent private(continentName: String) extends AnyVal
+object Continent {
   def new_continent(x: Option[String]): Option[Continent] = {
     val pattern = """^AF|AN|AS|EU|NA|OC|SA$""".r
     x match {
@@ -44,14 +44,14 @@ object continent {
   }
 }
 
-case class Internet_link private(link: String) extends AnyVal
-object internet_link {
-  def new_internet_link(x: Option[String]): Option[Internet_link] = {
+case class InternetLink private(link: String) extends AnyVal
+object InternetLink {
+  def new_internet_link(x: Option[String]): Option[InternetLink] = {
     val pattern = """^http[s]?:\/\/[a-z]*\.?[a-z]+[\.[a-z]+]?[\/[a-z]*]?$""".r
     x match {
       case None => None
       case Some(x) => x match {
-        case pattern(_*) => Some(Internet_link(x))
+        case pattern(_*) => Some(InternetLink(x))
         case _ => None
       }
     }
